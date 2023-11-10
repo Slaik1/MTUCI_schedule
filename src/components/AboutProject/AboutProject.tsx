51ce374558891c485d25db9e4eb4c47cefa72256
Viacheslav Kolmyk <108751046+Slaik1@users.noreply.github.com>
2023-11-10 03:21:02 +0300

    implement adaptive to AboutProject component

--- src/components/AboutProject/AboutProject.tsx
+++ src/components/AboutProject/AboutProject.tsx
@@ -5,10 +5,13 @@ import cl from './AboutProject.module.scss'
 const AboutProject:FC = () => {
     return (
         <div className={cl.wrapper}>
+            <h1>О проекте</h1>
             <p className={cl.text}>
                 Данный web сервис разработан для упрощения
                 просмотра рассписания пар университета СКФ МТУСИ.
+                Если вы увидели некорректную работу сайта сообщите о ней. Это улучшит работу сайта.
             </p>
+            <a href="https://github.com/Slaik1/MTUCI_schedule/issues">Сообщить об ошибке</a>
             <div className={cl.icons}>
                 <ReactSVG src='./svg/about/react.svg'/>
                 <ReactSVG src='./svg/about/typescript.svg'/>
